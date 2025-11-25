6bcf22bf04a647cc17083e46d8a2d18350038325
Tushar-Deshmukh <tushardeshmukh57985@gmail.com>
2025-11-25 23:49:31 +0530

    covered adding background linear gradient , background image and alert api

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,6 +1,8 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton({ children }) {
+function PrimaryButton({ children,onPress }) {
+
+
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
@@ -10,6 +12,7 @@ function PrimaryButton({ children }) {
             : styles.buttonInnerContainer
         }
         android_ripple={{ color: "#640233" }}
+        onPress={onPress}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,28 +1,52 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
 function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState("");
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Invalid Number", "Number has to be in between 1 and 99", [
+        {
+          text: "Okay",
+          style: "destructive",
+          onPress: resetInputHandler,
+        },
+      ]);
+      return;
+    }
+  }
+
   return (
     <View style={styles.inputContainer}>
-        
       <TextInput
         style={styles.numberInput}
         maxLength={2}
         keyboardType="number-pad"
+        value={enteredNumber}
+        onChange={numberInputHandler}
       />
 
       <View style={styles.buttonsContainer}>
-
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
 
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
-
       </View>
-
     </View>
   );
 }
